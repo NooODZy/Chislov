0cade55e661951d59a29933eb178ed2e37f335ed
NooODZy <76495437+NooODZy@users.noreply.github.com>
2021-12-29 00:57:56 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,28 @@
+"""
+:authors: Fedor987
+
+Модули содержат в себе пакеты с решенными ТЗ.
+Каждый пакет соответствует каждому из ТЗ.
+
+"""
+
+from .TZ1 import *
+from .TZ2 import *
+from .TZ3 import *
+from .TZ6 import *
+from .TZ7Otjig import *
+from .TZ8Ants import *
+
+
+__author__ = 'Fedor987'
+__version__ = '0.7'
+
+print("""Модуль разделен на пакеты:
+        TZ1
+        TZ2
+        TZ3
+        TZ6
+        TZ7
+        TZ8
+      """)
+
